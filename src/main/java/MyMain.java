6983fb97b003a38feb557c59b69fd56e4c08e725
nbutcher1 <22nbutcher@athenian.org>
2020-09-30 05:51:49 +0000

    donde

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,20 +2,44 @@ public class MyMain {
 
     // Reverses an array
     public static int[] reverse(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int[] finished = new int[arr.length];
+        finished[0] = arr[(arr.length-1)];
+        finished[(arr.length-1)] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            finished[i] = arr[(arr.length-i-1)];
+        }
+        return finished;
     }
 
     // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int largest = arr[0];
+        int place = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+                place = i;
+            }
+        }
+
+        int second = -1000000;
+        for (int i = 0; i < arr.length; i++) {
+            if (place != i && arr[i] > second) {
+                second = arr[i];
+            }
+        }
+        return second;
     }
 
     // Checks to see if an array contains a geometric series
     public static boolean isGeometric(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return false;
+        double counter = (double) arr[1] / (double) arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if ((double) arr[i] / counter != (double) arr[i-1]) {
+                return false;
+        }
+    }
+    return true;
     }
 
 
